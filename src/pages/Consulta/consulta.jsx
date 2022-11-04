ce69909ee5d328f372e8e4ee4a555342a33694b6
RivaniaGomes <rivaniagomes@gmail.com>
2022-11-04 16:11:57 -0300

    Resolvendo conflito

--- src/pages/Consulta/consulta.jsx
+++ src/pages/Consulta/consulta.jsx
@@ -8,8 +8,6 @@ function Consulta() {
 
     const [produto, setProduto] = useState([])
 
-
-    //Só é executado após o componente ser renderizado
     useEffect(() => {
         getProduto();
         console.log(produto)
@@ -19,9 +17,7 @@ function Consulta() {
         const { data } = await api.get("/produtos")
         setProduto(data)
     }
-/*     const popover = new bootstrap.Popover('.example-popover', {
-        container: 'body' */
-      //})
+
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
@@ -29,18 +25,6 @@ function Consulta() {
     return (
         <>
             <Header />
-<<<<<<< HEAD
-            <div className="container w-80 p-3 align-items-center">
-            {produto.length > 0 ? (
-                <>
-                    {produto.map((item) =>
-                        <div className="card" style={{ width: "18rem" }} key={item.id}>
-                            <img src={item.foto} className="card-img-top" alt="carros" />
-                            <div className="card-body">
-                                <h5 className="card-title">{item.nome}</h5>
-                                <p className="card-text">{item.valor}</p>
-                                <a href="#" className="btn btn-primary">Detalhes</a>
-=======
             <div className="container w-100 p-3 align-items-center" >
                 {produto.length > 0 ? (
                     <>
@@ -50,22 +34,16 @@ function Consulta() {
                                 <div className="card-body">
                                     <h5 className="card-title">{item.nome}</h5>
                                     <p className="card-text">{item.valor}</p>
-
                                     <a tabindex="0" className="btn btn-lg btn-danger" role="button" data-bs-toggle="popover" data-bs-trigger="focus" data-bs-title="Carro bolado" data-bs-content="habdfbfduhbauswfayfbrwtefyvfewtvfwtefvtefwvewtyfvewfytv">Detalhes</a>
-
-
                                 </div>
->>>>>>> 3dc8572c5463d474d01538ba75c897572efbf826
                             </div>
                         )}
                     </>
                 ) : (
                     <p>carregando ...</p>
                 )}
-
             </div>
             <Footer />
-
         </>
     )
 }
