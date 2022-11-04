3dc8572c5463d474d01538ba75c897572efbf826
CleiverSoares <cleiversoares2@gmail.com>
2022-11-04 15:53:20 -0300

    remoção da paginação

--- src/pages/Consulta/consulta.jsx
+++ src/pages/Consulta/consulta.jsx
@@ -49,15 +49,7 @@ function Consulta() {
                 ) : (
                     <p>carregando ...</p>
                 )}
-                <nav aria-label="Page navigation example">
-                    <ul className="pagination">
-                        <li className="page-item"><a className="page-link" href="#">Anterior</a></li>
-                        <li className="page-item"><a className="page-link" href="#">1</a></li>
-                        <li className="page-item"><a className="page-link" href="#">2</a></li>
-                        <li className="page-item"><a className="page-link" href="#">3</a></li>
-                        <li className="page-item"><a className="page-link" href="#">Próximo</a></li>
-                    </ul>
-                </nav>
+
             </div>
             <Footer />
 
