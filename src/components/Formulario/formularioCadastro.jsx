40437e5138ad600a33a5b0696395b824c673f082
CleiverSoares <cleiversoares2@gmail.com>
2022-11-07 23:37:19 -0300

    inicio da validação

--- src/components/Formulario/formularioCadastro.jsx
+++ src/components/Formulario/formularioCadastro.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import { Link, useLocation, useParams } from "react-router-dom";
-import api from "../../services/api"
-import './formularioCadastro.css'
+import { useLocation } from "react-router-dom";
+import api from "../../services/api";
+import './formularioCadastro.css';
+import Modal from '../Modal/modal'
 
 function FormularioCadastro() {
     const { state } = useLocation()
@@ -17,16 +18,23 @@ function FormularioCadastro() {
     }, [])
 
 
-    const cadastrarItem = async () => {
-        const itemAtualizado = {
-            veiculo: veiculo,
-            modelo: modelo,
-            fabricante: fabricante,
-            ano: ano,
-            valor: valor,
-            foto: foto
+    const cadastrarItem = async (e) => {
+        if (veiculo == "" || modelo == "" || fabricante == "" || ano == "" || valor == "" || foto == "") {
+            alert("Todos os campos devem ser preenchidos.")
+        } else {
+
+            <Modal />
         }
-        await api.post(`/carros/`, itemAtualizado)
+            const itemAtualizado = {
+                veiculo: veiculo,
+                modelo: modelo,
+                fabricante: fabricante,
+                ano: ano,
+                valor: valor,
+                foto: foto
+            }
+            await api.post(`/carros/`, itemAtualizado)
+        
     }
 
     return (
@@ -47,22 +55,22 @@ function FormularioCadastro() {
                         </div>
                         <div className="form-group col-md-5 d-inline-block m-3">
                             <label htmlFor="fabricante">Fabricante</label>
-                            <input type="text" className="form-control" id="fabricante" placeholder="Fabricante" onChange={e => setFabricante(e.target.value)} />
+                            <input type="text" className="form-control" id="fabricante" placeholder="Fabricante" onChange={e => setFabricante(e.target.value)} required />
                         </div>
                         <div className="form-group col-md-1 d-inline-block m-3">
                             <label htmlFor="ano">Ano</label>
-                            <input type="text" className="form-control" id="ano" placeholder="Ano" onChange={e => setAno(e.target.value)} />
+                            <input type="number" className="form-control" id="ano" min={2010} placeholder="Ano" onChange={e => setAno(e.target.value)} required />
                         </div>
                         <div className="form-group col-md-2 d-inline-block m-3">
                             <label htmlFor="valor">Valor</label>
-                            <input type="text" className="form-control" id="valor" placeholder="Valor" onChange={e => setValor(e.target.value)} />
+                            <input type="text" className="form-control" id="valor" placeholder="Valor" onChange={e => setValor(e.target.value)} required />
                         </div>
                         <div className="form-group col-md-9 d-inline-block m-3">
                             <label htmlFor="ano">Foto</label>
-                            <input type="text" className="form-control" id="foto" placeholder="Url da foto" onChange={e => setFoto(e.target.value)} />
+                            <input type="text" className="form-control" id="foto" placeholder="Url da foto" onChange={e => setFoto(e.target.value)} required />
                         </div>
                     </div>
-                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={cadastrarItem}>Cadastrar</button>
+                    <button type="submit" className="btn btn-primary " data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={cadastrarItem}>Cadastrar</button>
                 </form>
             </div>
         </>
