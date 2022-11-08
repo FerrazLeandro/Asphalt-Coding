698708abd9536dac7efadd9a54d50016747f8093
Leandro Ferraz <85909017+FerrazLeandro@users.noreply.github.com>
2022-11-07 21:55:50 -0300

    Ajustes de responsividade

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -6,11 +6,11 @@ function Header() {
         <>
             <nav className="navbar fixed-top navbar-expand-lg bg-dark navbar-dark">
                 <div className="container-fluid">
-                    <a class="navbar-brand" href="#"><Link className="nav-link" to='/'>Asphalt Coding</Link></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <Link className="navbar-brand" to='/'>Asphalt Coding</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
